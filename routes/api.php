3e8d02c56fb87c50541ff8253c3b05792c234a95
quangvulc2001@gmail.com <116878580+quangvu0901@users.noreply.github.com>
2023-01-12 09:45:33 +0700

    12012023945

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware(['api','auth','verified','check_user'])->prefix('/')->group(function (){
+
+});
+
+Route::middleware('api')->prefix('/abc')->group(function (){
+    Route::post('/1',[\App\Http\Controllers\AuthController::class,'login']);
+});
+
